d190a74a015c6a9f174b4a64b11cd1b7026d24f4
yurygabriel <110116044+yurygabriel@users.noreply.github.com>
2023-09-18 08:09:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function mensagem (msg) {
-    mensagem = (msg) => alert(msg);
-    soma = (a,b) => a + b;
-  }
+  mensagem = (msg) => alert(msg);
+  soma = (a,b) => a + b;
+  sub = (a,b) => a - b;
+  mult = (a,b) => a * b;
+  div = (a,b) => a / b;
+  raiz = x => Math.sqrt(x);
+//mensagem( soma( 10, soma(8,2) ) );
+//mensagem(soma(2,div(2,2)));
+//mensagem(soma(div(80,2),mult(5,3)));
+//mensagem(soma(sub(8,5),sub(3,70)))
+}
