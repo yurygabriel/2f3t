97aedbe9e30ec178db48953415f0c467b38524a8
yurygabriel <110116044+yurygabriel@users.noreply.github.com>
2023-10-02 08:29:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-function mensagem (msg) {
-  mensagem = (msg) => alert(msg);
-  soma = (a,b) => a + b;
-  sub = (a,b) => a - b;
-  mult = (a,b) => a * b;
-  div = (a,b) => a / b;
-  raiz = x => Math.sqrt(x);
-//mensagem( soma( 10, soma(8,2) ) );
-//mensagem(soma(2,div(2,2)));
-//mensagem(soma(div(80,2),mult(5,3)));
-//mensagem(soma(sub(8,5),sub(3,70)))
+mensagem = (msg) => alert(msg);
+soma = (a,b) => a + b;
+sub = (a,b) => a - b;
+mult = (a,b) => a * b;
+div = (a,b) => a / b;
+raiz = X => Math.sqrt(X);
+let a = "";
+let b = "";
+let valor = "";
+let temPonto = false
+
+function escrever_display(num){
+  document.getElementById("resultado").value = num;
+
+}
+function digitando(tecla){
+     if( tecla == "."){
+       if(!temPonto){
+         temPonto = true;
+         valor += tecla
+         escrever_display(valor)
+    }
+    return;
+  }
+valor += tecla
+escrever_display(valor);
+
+}
+let executa = "";
+function operacao(op){
+  executa = op;
+  a = valor;
+  valor = "";
+  temPonto = false; 
+}
+function calcula(){
+  if(executa != "")
+  b=valor;
+  if(executa == "mult") escrever_display(mult(a,b));
+  if(executa == "div") escrever_display(div(a,b));
+  if(executa == "sub") escrever_display(sub(a,b));
+  if(executa == "soma") escrever_display(soma(Number(a), Number(b)));
+  a = "";
+  b = "";
+  valor = "";
+  temPonto = false;
 }
